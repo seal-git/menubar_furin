3c7384f9fc293ecb2ae4cab5b6543877f996379b
nakamuratoru <toru.n.2567@gmail.com>
2022-08-07 13:18:53 +0900

    生成ファイル修正

--- create_sound/src/mp3_creator.py
+++ create_sound/src/mp3_creator.py
@@ -7,9 +7,9 @@ from pydub import AudioSegment
 
 args = sys.argv
 input_file = args[1]
-output_mp3 = args[2]
-db = args[3]
-num = args[4]
+output_num = int(args[2])
+db = float(args[3])
+num = int(args[4])
 sign = args[5]
 
 ext = os.path.splitext(input_file)[1][1:]
@@ -18,12 +18,14 @@ base_sound = AudioSegment.from_file(input_file, format=ext)  # 音声を読み�
 for i in range(int(num)):
     if (i == 0):
         edit_sound = base_sound
-        edit_sound.export(f"../mp3/{output_mp3}_{i}.mp3", format="mp3")
+        edit_sound.export(f"../mp3/{output_num}.mp3", format="mp3")
     else:
         if (sign == "plus"):
-            edit_sound = base_sound + float(db)
-            edit_sound.export(f"../mp3/{output_mp3}_{i}.mp3", format="mp3")
+            filenum = output_num + i
+            edit_sound = base_sound + db
+            edit_sound.export(f"../mp3/{filenum}.mp3", format="mp3")
         elif (sign == "minus"):
-            edit_sound = base_sound - float(db)
-            edit_sound.export(f"../mp3/{output_mp3}_-{i}.mp3", format="mp3")
+            filenum = output_num - i
+            edit_sound = base_sound - db
+            edit_sound.export(f"../mp3/{filenum}.mp3", format="mp3")
 
